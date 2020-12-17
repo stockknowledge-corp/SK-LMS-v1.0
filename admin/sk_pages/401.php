42cbbdee2f04e5f92fc9c1ccae916c4ed31d24fd
Adrian Cleofe <adriancleofe@gmail.com>
2020-12-17 16:30:21 +0800

    Unauthorize page

---
+++ admin/sk_pages/401.php
@@ -0,0 +1,21 @@
+<?php
+require_once("../_class.dba.inc.php");
+require_once("../_conf.dba.inc.php");
+require_once("../_static.session.inc.php");
+validate_session();
+?>
+<html>
+<head>
+<title>Privacy Policy</title>
+<link rel="stylesheet" type="text/css" media="screen" href="../style.css" />
+<?php include('../header.php');?>
+
+</head>
+<body class="tos">
+<?php include('../sidebar.php');?>
+<div id="main-container"><div id="main">
+<h1>Error 401</h1>
+
+<?php include('../footer.php');?>
+</body>
+</html> 
