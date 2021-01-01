492542105b03746cb0846f89dbdf3803290e39cf
jcwebhole <jcwebhole@yahoo.com>
2021-01-02 00:31:37 +0800

    closes #9; skip foreign key check for user removal

--- admin/sk_users/remove.php
+++ admin/sk_users/remove.php
@@ -7,8 +7,18 @@ validate_session();
 
 $id = mysqli_real_escape_string($dba->link_id,$_REQUEST['id']);
 
+$query = "SET FOREIGN_KEY_CHECKS=OFF;";
+$dba->query($query);
+
+$query = "UPDATE `sk_history` SET `user_id` = '1' WHERE `user_id`='".$id."'";
+$dba->query($query);
+
 $query = "DELETE FROM sk_users WHERE id='".$id."' LIMIT 1";
 $dba->query($query);
+
+$query = "SET FOREIGN_KEY_CHECKS=ON;";
+$dba->query($query);
+
 
 $_SESSION['flash'] = "Entry number ".$id." deleted.";
 
